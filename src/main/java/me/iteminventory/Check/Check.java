dd8cfb1c47d2f8ed3129f45f00acdffa29621904
kimpro12 <95573884+kimpro12@users.noreply.github.com>
2023-01-15 12:58:51 +0700

    Add files via upload

--- src/main/java/me/iteminventory/Check/Check.java
+++ src/main/java/me/iteminventory/Check/Check.java
@@ -9,6 +9,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class Check {
@@ -22,47 +23,76 @@ public class Check {
         new Task() {
             @Override
             public void run() {
-                check1();
+                for (Player p : Bukkit.getOnlinePlayers()) {
+                    check1(p);
+                }
             }
         }.runTaskTimer(plugin, 20, 20);
     }
 
-    public void check1() {
+    public void check1(Player p) {
         Set<String> all = plugin.getConfig().getConfigurationSection("itemlist").getKeys(false);
         for (String idk : all) {
             try {
-                for (Player p : Bukkit.getOnlinePlayers()) {
-                    Material m = Material.matchMaterial(idk);
-                    ItemStack i = new ItemStack(m);
-                    ItemMeta meta = i.getItemMeta();
-                    ArrayList<String> lore = new ArrayList<>();
-                    lore.add(plugin.getConfig().getString("itemlist." + idk + ".lore"));
-                    meta.setLore(lore);
-                    meta.setDisplayName(plugin.getConfig().getString("itemlist." + idk + ".displayname"));
-                    String lore1 = plugin.getConfig().getString("itemlist." + idk + ".lore");
-                    if (lore1.equalsIgnoreCase("none")) {
-                        meta.setLore(null);
-                        lore.remove(lore1);
-                    }
-                    String displayename1 = plugin.getConfig().getString("itemlist." + idk + ".displayname");
-                    if (displayename1.equalsIgnoreCase("none")) {
-                        meta.setDisplayName(null);
-                        lore.remove(displayename1);
-                    }
-                    i.setItemMeta(meta);
-                    lore.clear();
-                    if (hasItem(p, i)) {
-                        for (String idk2 : plugin.getConfig().getStringList("command")) {
-                            if (idk2.contains("%player")) {
-                                String idk3 = idk2.replace("%player", p.getDisplayName());
-                                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), idk3);
-                            } else {
-                                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), idk2);
+                Material m = Material.matchMaterial(idk);
+                ItemStack i = new ItemStack(m);
+                ItemMeta meta = i.getItemMeta();
+                ArrayList<String> lore = new ArrayList<>();
+                lore.add(plugin.getConfig().getString("itemlist." + idk + ".lore"));
+                meta.setLore(lore);
+                meta.setDisplayName(plugin.getConfig().getString("itemlist." + idk + ".displayname"));
+                String lore1 = plugin.getConfig().getString("itemlist." + idk + ".lore");
+                if (lore1.equalsIgnoreCase("none")) {
+                    meta.setLore(null);
+                    lore.remove(lore1);
+                }
+                String displayename1 = plugin.getConfig().getString("itemlist." + idk + ".displayname");
+                if (displayename1.equalsIgnoreCase("none")) {
+                    meta.setDisplayName(null);
+                    lore.remove(displayename1);
+                }
+                i.setItemMeta(meta);
+                lore.clear();
+                if (hasItem(p, i)) {
+                    List<String> stringList = plugin.getConfig().getStringList("command");
+                    for (String idk2 : stringList) {
+                        if (idk2.contains("%player")) {
+                            String idk3 = idk2.replace("%player", p.getDisplayName());
+                            if (idk2.contains("%op")) {
+                                String idk4 = idk3.replace("%op", getOperators().getDisplayName());
+                                if (idk2.contains("%item")) {
+                                    String idk5 = idk4.replace("%item", i.getType().name());
+                                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), idk5);
+                                    continue;
+                                }
+                                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), idk4);
+                                continue;
                             }
+                            if (idk2.contains("item")) {
+                                String idk5 = idk3.replace("%item", i.getType().name());
+                                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), idk5);
+                                continue;
+                            }
+                            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), idk3);
+                            continue;
                         }
-                    } else {
-
+                        if (idk2.contains("%op") && !(idk2.contains("%player"))) {
+                            String idk4 = idk2.replace("%op", getOperators().getDisplayName());
+                            if (idk2.contains("item")) {
+                                String idk5 = idk4.replace("%item", i.getType().name());
+                                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), idk5);
+                                continue;
+                            }
+                            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), idk4);
+                            continue;
+                        }
+                        if (idk2.contains("%item") && !(idk2.contains("%player")) && !(idk2.contains("%op"))) {
+                            String idk5 = idk2.replace("%item", i.getType().name());
+                            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), idk5);
+                        }
                     }
+                } else {
+
                 }
             } catch (Exception e) {
 
@@ -83,6 +113,7 @@ public class Check {
         }
         return false;
     }
+
     public boolean isEqual(ItemStack i1, ItemStack i2) {
         if (i1.getType().equals(i2.getType())) {
             if (i2.getItemMeta().hasLore()) {
@@ -107,7 +138,7 @@ public class Check {
                 }
             }
             if (i1.getItemMeta().hasLore()) {
-                if(!(i2.getItemMeta().hasLore())) {
+                if (!(i2.getItemMeta().hasLore())) {
                     return false;
                 }
             }
@@ -127,5 +158,14 @@ public class Check {
             return true;
         }
         return false;
+    }
+
+    public Player getOperators() {
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            if (p.isOp()) {
+                return p;
+            }
+        }
+        return null;
     }
 }
